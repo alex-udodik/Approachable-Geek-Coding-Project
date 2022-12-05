aa5dbe155a6b8d03722a43f017f0c92345910d59
Alex <alex@Alexs-MacBook-Air.local>
2022-12-05 13:36:10 -0800

    Added temporary buttons for screen transitions. Added ViewControllers to edit data

---
+++ Approachable Geek Coding Project/Approachable Geek Coding Project/EditNameController.swift
@@ -0,0 +1,21 @@
+//
+//  EditNameController.swift
+//  Approachable Geek Coding Project
+//
+//  Created by Alex on 12/5/22.
+//
+
+import Foundation
+import UIKit
+
+class EditNameController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
---
+++ Approachable Geek Coding Project/Approachable Geek Coding Project/EditProfileHomeController.swift
@@ -0,0 +1,22 @@
+//
+//  EditProfileHomeController.swift
+//  Approachable Geek Coding Project
+//
+//  Created by Alex on 12/5/22.
+//
+import UIKit
+import Foundation
+
+
+class EditProfileHomeController: UIViewController {
+
+    @IBOutlet weak var EditAboutMeLabel: UILabel!
+    @IBOutlet weak var EditEmailLabel: UILabel!
+    @IBOutlet weak var EditNumberLabel: UILabel!
+    @IBOutlet weak var EditNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
